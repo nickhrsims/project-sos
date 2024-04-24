45d348c694197932610d664d2364224347b4fcf9
Nicholas H.R. Sims <nickhrsims@gmail.com>
2024-04-24 21:45:36 +0000

    feat: pseudo-generated additional keys

--- src/input/keyboard.generated.h
+++ src/input/keyboard.generated.h
@@ -2,6 +2,8 @@
 #include "SDL_scancode.h"
 enum class keyboard {
   key_a = SDL_SCANCODE_A,
+  key_b = SDL_SCANCODE_B,
+  key_c = SDL_SCANCODE_C,
   key_z = SDL_SCANCODE_Z,
   key_k = SDL_SCANCODE_K,
   key_m = SDL_SCANCODE_M,
