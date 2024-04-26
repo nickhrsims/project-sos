6ef2ca383dbf602e4a92853d48d62770b9619621
Nicholas H.R. Sims <nickhrsims@gmail.com>
2024-04-26 17:26:01 +0000

    chore: quarentine main

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,66 @@
-#include "sos.h"
 #include <cstdlib>
-
 int main() {
+  /*
+    // ---------------------------------
+    // Initialize
+    // ---------------------------------
 
-  // ---------------------------------
-  // Initialize
-  // ---------------------------------
+    sos::root::init({
+        false,
+        {"Example SOS App", 64, 64, 200, 200},
+        {},
+    });
 
-  sos::root::init({
-      false,
-      {"Example SOS App", 64, 64, 200, 200},
-      {},
-  });
+    // ---------------------------------
+    // Player Input Configuration
+    // ---------------------------------
 
-  // ---------------------------------
-  // Player Input Configuration
-  // ---------------------------------
+    // --- Player Data & Actions
+    int p1_pos = 0;
+    int p2_pos = 0;
 
-  // --- Player Data & Actions
-  int p1_pos = 0;
-  int p2_pos = 0;
+    enum class control { quit, report };
+    enum class player_1 { up, down };
+    enum class player_2 { up, down };
 
-  enum class control { quit, report };
-  enum class player_1 { up, down };
-  enum class player_2 { up, down };
+    // --- Relate actions to an input type
+    sos::input::action(control::quit, sos::input::keyboard::key_q);
+    sos::input::action(control::report, sos::input::mouse::button_left);
+    sos::input::action(player_1::up, sos::input::keyboard::key_a);
+    sos::input::action(player_1::down, sos::input::keyboard::key_z);
+    sos::input::action(player_2::up, sos::input::keyboard::key_k);
+    sos::input::action(player_2::down, sos::input::keyboard::key_m);
 
-  // --- Relate actions to an input type
-  sos::input::action(control::quit, sos::input::keyboard::key_q);
-  sos::input::action(control::report, sos::input::mouse::button_left);
-  sos::input::action(player_1::up, sos::input::keyboard::key_a);
-  sos::input::action(player_1::down, sos::input::keyboard::key_z);
-  sos::input::action(player_2::up, sos::input::keyboard::key_k);
-  sos::input::action(player_2::down, sos::input::keyboard::key_m);
+    // --- Input Subscription
+    [[maybe_unused]] auto player_any_input_subscription =
+        sos::input::on<control>([&](auto action) {
+          switch (action) {
+          case control::quit:
+            sos::process::stop();
+            break;
+          case control::report:
+            spdlog::info("Player 1 :: {} | Player 2 :: {}", p1_pos, p2_pos);
+            break;
+          default:
+            break;
+          }
+        });
 
-  // --- Input Subscription
-  [[maybe_unused]] auto player_any_input_subscription =
-      sos::input::on<control>([&](auto action) {
-        switch (action) {
-        case control::quit:
-          sos::process::stop();
-          break;
-        case control::report:
-          spdlog::info("Player 1 :: {} | Player 2 :: {}", p1_pos, p2_pos);
-          break;
-        default:
-          break;
-        }
-      });
+    // ---------------------------------
+    // Primary Simulation Loop
+    // ---------------------------------
 
-  // ---------------------------------
-  // Primary Simulation Loop
-  // ---------------------------------
+    sos::process::start([&](float) {
+      using sos::input::action;
+      p1_pos = action(player_1::down) - action(player_1::up);
+      p2_pos = action(player_2::down) - action(player_2::up);
+    });
 
-  sos::process::start([&](float) {
-    using sos::input::action;
-    p1_pos = action(player_1::down) - action(player_1::up);
-    p2_pos = action(player_2::down) - action(player_2::up);
-  });
+    // ---------------------------------
+    // Cleanup
+    // ---------------------------------
 
-  // ---------------------------------
-  // Cleanup
-  // ---------------------------------
-
-  sos::root::quit();
-
+    sos::root::quit();
+  */
   return EXIT_SUCCESS;
 }
