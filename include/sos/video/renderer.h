2a1017b7c038e75b0c11717643fd6536a6111deb
Nicholas H.R. Sims <nickhrsims@gmail.com>
2024-05-22 01:17:23 +0000

    feat(renderer): concept support for point, rect, and color

--- include/sos/video/renderer.h
+++ include/sos/video/renderer.h
@@ -2,11 +2,29 @@
 
 #include "SDL2/SDL.h"
 
+#include <sos/video/color.h>
 #include <sos/video/font.h>
 #include <sos/video/texture.h>
 #include <sos/video/window.h>
 
 namespace sos::video::renderer {
+
+// -----------------------------------------------------------------------------
+// Concepts
+// -----------------------------------------------------------------------------
+
+template <typename T>
+concept SdlCompatibleRect = std::is_base_of<SDL_Rect, T>::value;
+
+template <typename T>
+concept SdlCompatibleColor = std::is_base_of<SDL_Color, T>::value;
+
+template <typename T>
+concept SdlCompatiblePoint = std::is_base_of<SDL_Point, T>::value;
+
+// -----------------------------------------------------------------------------
+// Functions
+// -----------------------------------------------------------------------------
 
 template <uint8_t index = 0> void create();
 
@@ -16,16 +34,16 @@ template <uint8_t index = 0> void clear();
 
 template <uint8_t index = 0> void show();
 
-template <uint8_t index = 0, typename Color> void set_color(const Color &color);
+template <uint8_t index = 0, SdlCompatibleColor Color>
+void set_color(const Color& color);
 
-template <uint8_t index = 0, typename Rect> void draw(const Rect &rect);
+template <uint8_t index = 0, SdlCompatibleRect Rect> void draw(const Rect& rect);
 
-template <uint8_t index = 0, typename Position>
-void draw(const texture &texture, const Position &position);
+template <uint8_t index = 0, SdlCompatiblePoint Position>
+void draw(const texture& texture, const Position& position);
 
-template <uint8_t index = 0, typename Color>
-texture load_texture(const font &font, const Color &color,
-                     const std::string &text);
+template <uint8_t index = 0, SdlCompatibleColor Color>
+texture load_texture(const font& font, const Color& color, const std::string& text);
 
 } // namespace sos::video::renderer
 
