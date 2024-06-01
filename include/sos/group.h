c4429b64da69649929a83d27836c0869302a7cb1
Nicholas H.R. Sims <nickhrsims@gmail.com>
2024-06-01 01:14:25 +0000

    refactor!: replace destructor with call operator for unsubscribe
    As a rule of thumb, interfaces should be easy to use correctly, and hard to use incorrectly.
    This particular unit came up as a subject of confusion, causing a difficult to diagnose bug.
    
    This revision models more closely to firestoreJs #onSnapshot et al.

--- include/sos/group.h
+++ include/sos/group.h
@@ -12,50 +12,50 @@ namespace sos {
  * (Just a list wrapper with element lifecycle awareness).
  */
 template <typename T> class group {
-public:
-  /**
-   * Value type for reference.
-   */
-  using value_type = T;
-  /**
-   * A handle representing member participation lifecycle.
-   *
-   * Moving out of scope will forfeit membership and execution participation.
-   *
-   * Does not support copy or move by design. Lifetime of membership must
-   * be shorter than the lifetime of the registered participant.
-   */
-  struct membership {
-    friend group<T>;
-    membership(group<T> &p_group_obj, std::list<T>::iterator p_iterator)
-        : group_{p_group_obj}, iterator_{p_iterator} {}
-    ~membership() { group_.revoke(*this); };
+  public:
+    /**
+     * Value type for reference.
+     */
+    using value_type = T;
+    /**
+     * A handle representing member participation lifecycle.
+     *
+     * Moving out of scope will forfeit membership and execution participation.
+     *
+     * Does not support copy or move by design. Lifetime of membership must
+     * be shorter than the lifetime of the registered participant.
+     */
+    struct membership {
+        friend group<T>;
+        membership(group<T>& p_group_obj, std::list<T>::iterator p_iterator)
+            : group_{p_group_obj}, iterator_{p_iterator} {}
+        void operator()() { group_.revoke(*this); };
 
-    // --- Cannot copy or move
-    membership(const membership &) = delete;
-    membership(membership &&) = delete;
-    membership &operator=(const membership &) = delete;
-    membership &&operator=(membership &&) = delete;
+        // --- Cannot copy or move
+        membership(const membership&)            = delete;
+        membership(membership&&)                 = delete;
+        membership& operator=(const membership&) = delete;
+        membership&& operator=(membership&&)     = delete;
 
-  private:
-    group<T> &group_;
-    std::list<T>::iterator iterator_;
-  };
+      private:
+        group<T>& group_;
+        std::list<T>::iterator iterator_;
+    };
 
-  /**
-   * Issue a membership token to an execution participant.
-   */
-  membership issue(T t) {
-    return membership{*this, participants_.insert(participants_.end(), t)};
-  }
+    /**
+     * Issue a membership token to an execution participant.
+     */
+    membership issue(T t) {
+        return membership{*this, participants_.insert(participants_.end(), t)};
+    }
 
-  /**
-   * Revoke an execution participation token.
-   */
-  void revoke(membership &m) { participants_.erase(m.iterator_); }
+    /**
+     * Revoke an execution participation token.
+     */
+    void revoke(membership& m) { participants_.erase(m.iterator_); }
 
-protected:
-  std::list<T> participants_;
+  protected:
+    std::list<T> participants_;
 };
 
 } // namespace sos
