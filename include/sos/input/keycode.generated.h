1ee743332c15dd3e0b1dcc29089a6f737db279fe
Nicholas H.R. Sims <nickhrsims@gmail.com>
2024-06-01 01:10:00 +0000

    formatting: pseudo-generated files

--- include/sos/input/keycode.generated.h
+++ include/sos/input/keycode.generated.h
@@ -2,14 +2,14 @@
 
 namespace sos::input::keyboard {
 enum class keycode {
-  key_a = SDLK_a,
-  key_b = SDLK_b,
-  key_c = SDLK_c,
+    key_a = SDLK_a,
+    key_b = SDLK_b,
+    key_c = SDLK_c,
 
-  key_k = SDLK_k,
-  key_m = SDLK_m,
-  key_q = SDLK_q,
+    key_k = SDLK_k,
+    key_m = SDLK_m,
+    key_q = SDLK_q,
 
-  key_z = SDLK_z,
+    key_z = SDLK_z,
 };
 }
--- include/sos/input/scancode.generated.h
+++ include/sos/input/scancode.generated.h
@@ -1,13 +1,14 @@
 #include "SDL2/SDL.h"
+#include "SDL_scancode.h"
 namespace sos::input::keyboard {
 enum class scancode {
-  scancode_a = SDL_SCANCODE_A,
-  scancode_b = SDL_SCANCODE_B,
-  scancode_c = SDL_SCANCODE_C,
-  scancode_z = SDL_SCANCODE_Z,
-  scancode_k = SDL_SCANCODE_K,
-  scancode_m = SDL_SCANCODE_M,
-  scancode_q = SDL_SCANCODE_Q,
+    scancode_a = SDL_SCANCODE_A,
+    scancode_b = SDL_SCANCODE_B,
+    scancode_c = SDL_SCANCODE_C,
+    scancode_z = SDL_SCANCODE_Z,
+    scancode_k = SDL_SCANCODE_K,
+    scancode_m = SDL_SCANCODE_M,
+    scancode_q = SDL_SCANCODE_Q,
 
 };
 }
